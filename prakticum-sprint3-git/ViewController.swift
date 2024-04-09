0088120ac932e3a95708cc143370804e503c3619
xcode73 <nikolai.dev@icloud.com>
2024-04-09 13:21:53 +0400

    Initial Commit

---
+++ prakticum-sprint3-git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prakticum-sprint3-git
+//
+//  Created by Nikolai Eremenko on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
